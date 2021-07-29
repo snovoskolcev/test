8bd80af7cb71c0b6ebc3907a66e57ced0ac47b7a
slavik <slavi4ek64@mail.ru>
2021-07-29 10:06:13 +0300

    test success

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="container__left">
+        <input type="text" v-model="linkImg">
+        <button @click="setImage(linkImg)">Ok</button>
+      </div>
+      <div class="container__right">
+        <img :src="showImg.src" alt="picture" v-if="showImg.src">
+        <div v-else>Введите ссылку на изображение</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="js">
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      linkImg:"",
+      showImg:new Image()
+    }
+  },
+  methods:{
+    setImage(link){
+      const img = new Image();
+      img.onload = function () {
+        if(this.height > this.width){
+          this.width = this.width/5;
+          this.height = this.height/4;
+        }
+        if(this.width > this.height){
+          this.width = this.width/16;
+          this.height = this.height/9;
+        }
+      }
+      img.onerror = () => {
+        this.showImg = new Image();
+        this.linkImg = "";
+        alert('Неверно выбранна ссылка!');
+      }
+      img.src = link;
+      this.showImg = img;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container{
+  display: flex;
+  justify-content: center;
+  height:50vh;
+  width:100%;
+}
+.container__left{
+  flex:1;
+}
+input{
+  width:60%;
+}
+button{
+  width:20%;
+}
+.container__right{
+  flex:1;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+img{
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 </style>
